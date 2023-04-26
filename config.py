c6c1c81a2590e149fba7d57da4ead191b8ae802e
Nutty1704 <abhijitupadhyay03@gmail.com>
2023-04-26 16:02:22 +1000

    Add config file template

---
+++ config.py
@@ -0,0 +1,30 @@
+# Add your bot token here
+TOKEN = ''
+# Add your spotify client id here
+SPOTIFY_CLIENT_ID = ""
+# Add your spotify client secret id here
+SPOTIFY_CLIENT_SECRET_ID = ""
+
+PREFIX = '.'
+
+USER_NOT_IN_VOICE_CHANNEL = "Join a voice channel first in order to use this command!"
+USER_NOT_IN_SAME_VOICE_CHANNEL = "You are not in the voice channel I am playing in!"
+NO_AUDIO_IS_PLAYING = "No music is currently playing! Use *?play* to start playing some!"
+BOT_NOT_IN_VOICE_CHANNEL = "I wasn't present in any voice channel to begin with!"
+
+INVALID_INPUT = "No results were found."
+
+INVITE_LINK = "https://discord.com/api/oauth2/authorize?client_id=919635427332341831&permissions=3263552&scope=bot"
+
+
+# Error messages
+USER_NOT_IN_SAME_VOICE_CHANNEL = "You are not in the voice channel I am playing in!"
+USER_NOT_IN_VOICE_CHANNEL = "Join a voice channel first in order to use this command!"
+INVALID_INPUT = "No results were found."
+
+
+# store the message id of the now playing message
+MESSAGE_NOW_PLAYING = None
+PREVIOUS_TRACKS = []
+
+LOOPQ = None
