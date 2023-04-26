4c15f60218844e84764f8186e62693f49f15ca96
Nutty1704 <123238703+Nutty1704@users.noreply.github.com>
2023-04-26 16:23:21 +1000

    Remove invite link parameter

--- config.py
+++ config.py
@@ -14,8 +14,6 @@ BOT_NOT_IN_VOICE_CHANNEL = "I wasn't present in any voice channel to begin with!
 
 INVALID_INPUT = "No results were found."
 
-INVITE_LINK = "https://discord.com/api/oauth2/authorize?client_id=919635427332341831&permissions=3263552&scope=bot"
-
 
 # Error messages
 USER_NOT_IN_SAME_VOICE_CHANNEL = "You are not in the voice channel I am playing in!"
